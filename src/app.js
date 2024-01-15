260a3e279ec488beb70c4a3ecda785622a6dd818
Killianvdb <133092233+Killianvdb@users.noreply.github.com>
2024-01-15 23:17:26 +0100

    add: controlers

--- src/app.js
+++ src/app.js
@@ -44,6 +44,15 @@ app.post('/users', async (req, res) => {
   const { username,mail } = req.body;
   const result = await db.addUsers(username,mail);
   res.json(result);
+
+  // controleer of de email een @ bevat
+  if (mail.includes('@')) {
+    // als de email een @ bevat, dan is het een geldig email adres
+    console.log('Dit is een geldig email adres');
+  } else {
+    // als de email geen @ bevat, dan is het geen geldig email adres
+    console.log('Dit is geen geldig email adres');
+  }
 });
 
 //update user
@@ -52,6 +61,16 @@ app.put('/users/:id', async (req, res) => {
   const { username,mail } = req.body;
   const result = await db.updateUsers(id, username,mail);
   res.json(result);
+
+  // controleer of de email een @ bevat
+  if (mail.includes('@')) {
+    // als de email een @ bevat, dan is het een geldig email adres
+    console.log('Dit is een geldig email adres');
+  } else {
+    // als de email geen @ bevat, dan is het geen geldig email adres
+    console.log('Dit is geen geldig email adres');
+  }
+
 });
 
 //delete user
@@ -79,6 +98,16 @@ app.post('/messages', async (req, res) => {
   const { message,time_now } = req.body;
   const result = await db.addMessage(message,time_now);
   res.json(result);
+
+  // controleer als er een message is ingevuld
+  if (message) {
+    // als er een message is ingevuld, dan is het een geldige message
+    console.log('Dit is een geldige message');
+  } else {
+    // als er geen message is ingevuld, dan is het geen geldige message
+    console.log('Dit is geen geldige message');
+  }
+  
 });
 
 //update message
@@ -87,6 +116,15 @@ app.put('/messages/:id', async (req, res) => {
   const { message,time_now } = req.body;
   const result = await db.updateMessage(id, message,time_now);
   res.json(result);
+
+  // controleer als er een message is ingevuld
+  if (message) {
+    // als er een message is ingevuld, dan is het een geldige message
+    console.log('Dit is een geldige message');
+  } else {
+    // als er geen message is ingevuld, dan is het geen geldige message
+    console.log('Dit is geen geldige message');
+  }
 });
 
 //delete message
